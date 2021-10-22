88cd913b6872006e0ff36ffa125fce6cd31811bb
Fiona Meisner <meisnerfiona@gmail.com>
2021-10-21 17:31:19 -0700

    Completed ds-prep-example

---
+++ ds-prep-example/example.py
@@ -0,0 +1,18 @@
+print("""
+  ===============
+  ds-prep-example
+  ===============
+""")
+
+print("I would like to analyze {} to learn/predict {}.\n".format(
+
+  # write what you would like to analyze in the quotes here 👇
+
+  "a more in depth detail of data science",
+
+  # write what you would like to learn from that analysis here 👇
+
+  "more about the field of data science/how data science is used"
+
+
+))
