632ab0f58b685958ebec9e9c682b29f9a3f9349e
Fiona Meisner <meisnerfiona@gmail.com>
2021-10-23 16:18:03 -0700

    Completed ds-prep-python-variables

---
+++ ds-prep-python-variables/main.py
@@ -0,0 +1,36 @@
+first_name = 'Fiona'
+last_name = "Meisner"
+full_name = first_name + " " + last_name
+print(full_name)
+
+height_in_inches = 62
+print(type(height_in_inches), height_in_inches)
+height_in_inches_float = float(height_in_inches)
+print(type(height_in_inches_float), height_in_inches_float)
+
+height_in_meters = (height_in_inches_float/2.54)/100.0
+print(height_in_meters)
+
+eats_plants = True
+eats_animals = False
+
+is_animal = True if eats_plants or eats_animals == True else False
+is_omnivore = True if eats_plants and eats_animals == True else False
+is_plant = not is_animal
+
+print(is_animal, is_omnivore, is_plant)
+
+mean_height_in_meters = 1.7155
+short_threshold_in_meters = 1.576
+tall_threshold_in_meters = 1.860
+
+is_mean_height = (height_in_meters == mean_height_in_meters)
+is_short = (height_in_meters < tall_threshold_in_meters)
+is_tall = (height_in_meters >= tall_threshold_in_meters)
+is_normal_height = (height_in_meters >= short_threshold_in_meters and height_in_meters < tall_threshold_in_meters)
+
+print(is_mean_height, is_short, is_tall, is_normal_height)
+
+
+nothing = None
+print(nothing)
