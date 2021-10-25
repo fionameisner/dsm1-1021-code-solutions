b8e2a309541ae2189e263318163701f2bdcc972e
Fiona Meisner <meisnerfiona@gmail.com>
2021-10-24 21:07:52 -0700

    Completed ds-prep-python-functions.

---
+++ ds-prep-python-functions/main.py
@@ -0,0 +1,39 @@
+def function(value):
+  bool_to_int = lambda value: value = False
+function(value)
+# 2
+def function_two(a,b):
+  get_smaller = lambda a,b: min(a,b)
+function_two(1,5)
+# 3
+def cube(base):
+  base**3
+cube(5)
+# 4
+def absolute_difference(a,b):
+  x = abs(a,b)
+  print(x)
+absolute_difference(2,5)
+# 5
+def squared_difference(a,b):
+  x = a * b
+  print(x)
+squared_difference(3,11)
+# 6
+def hours_to_minutes(hours):
+  min = 60
+  return()
+hours_to_minutes(hours=1, min=60)
+# 7
+def get_circumference(radius):
+  return(radius)
+get_circumference(radius=360)
+# 8
+def linear_transform(x, slope, intercept):
+  return(slope,intercept)
+linear_transform(x=3,slope=1.5, intercept=3.2)
+# 9
+def standardize(x,x_center,scale_size):
+  x = ((x-x_center))/scale_size)
+  return(standardize)
+standardize(x =4, x_center=2, scale_size=6):
